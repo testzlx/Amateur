3f1524f5322c3f6f114919bfa3644eb0a342e3ed
zhanglinxing <zhanglinxing@meituan.com>
2018-01-02 20:32:27 +0800

    lrucache unfinished

---
+++ src/main/java/service/algorithm/LRUCache.java
@@ -0,0 +1,15 @@
+package service.algorithm;
+
+/**
+ * Created by zhanglinxing on 2018/1/2.
+ * 思路:使用链表（易于移动数据，查找都是根据key来，数组下标优势丧失）+capacity
+ */
+public class LRUCache {
+    int capacity;
+    public LRUCache(int capacity){
+        this.capacity = capacity;
+    }
+    public static void main(String[] args) {
+
+    }
+}
